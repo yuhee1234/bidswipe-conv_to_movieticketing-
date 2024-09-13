b28f65d23136471e87765c427325169ac0d7967d
yuhee1234 <143468727+yuhee1234@users.noreply.github.com>
2024-09-12 19:39:27 -0700

    Add files via upload

---
+++ BidSwipe/BidSwipeApp.swift
@@ -0,0 +1,31 @@
+//
+//  BidSwipeApp.swift
+//  BidSwipe
+//
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BidSwipeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
